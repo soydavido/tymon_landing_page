867b5113083b8973e0239b2ec0d0c7d444d9d8d4
Santiago Vieira M <88640435+santiagovm2000@users.noreply.github.com>
2025-11-30 19:35:45 -0400

    feat(footer): add smooth scrolling to sections from footer links

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppConfigService } from '../../services/app-config.service';
+import { scrollToSection } from '../../utils/scroll.utils';
 
 @Component({
   selector: 'app-footer',
@@ -12,4 +13,8 @@ import { AppConfigService } from '../../services/app-config.service';
 })
 export class FooterComponent {
   constructor(protected configService: AppConfigService) {}
+
+  scrollToSection(sectionId: string) {
+    scrollToSection(sectionId);
+  }
 }
